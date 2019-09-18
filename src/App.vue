e67b699cda8aaafc75b80395e118929838b53a52
cooper <cooper@BC-MG.localdomain>
2019-09-18 15:10:27 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span class="font-weight-light">Multi-Line Insurance Agency</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="http://www.multilineins.com/"
+        target="_blank"
+      >
+        <span class="mr-2">Go to Websight</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-container>
+    Form goes here...
+    <v-card>
+      <v-card-title>
+        <h3>Please tell us more about yourself.</h3>
+      </v-card-title>
+      <v-form class="px3 myform">
+        <v-text-field label="First Name" v-model="first"></v-text-field>
+        <v-text-field label="Last Name" v-model="last"></v-text-field>
+        <v-text-field label="Email"  v-model="email" ></v-text-field>
+        <v-textarea label="Additional Comments"  v-model="info" ></v-textarea>
+        <v-btn text class="success btn" @click="submit()">Submit</v-btn>
+      </v-form>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      first: "",
+      last: "",
+      email: "",
+      info: ""
+    }
+  },//end data
+  methods: {
+    submit() {
+      console.log ("Submitting: ", this.first, this.last );
+
+      var timestamp = new Date();
+
+      var infoObj = {
+        firstname: this.first,
+        lastname: this.last,
+        email: this.email,
+        info: this.info,
+        timestamp: timestamp
+      }
+
+      const url = `https://sleepy-everglades-99189.herokuapp.com/multiline`;
+
+      axios.post(url,infoObj)
+        .then(function (response) {
+          console.log("POST: ", response.data);
+
+        })
+        .catch(function (error) {
+          console.log("POST Error: ",  error);
+        });
+      
+          this.first = "";
+          this.last="";
+          this.email="";
+          this.info="";
+          
+    }//end submit
+  },//end methods
+};//end export
+</script>
+<style scoped>
+
+.myform {
+  padding: 1em;
+  background: #eee;
+}
+.btn {
+  padding: 1em;
+  float: right;
+  color: #fff;
+  width: 25%;
+  cursor: pointer;
+}
+</style>
